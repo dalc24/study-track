547d963f516222880f61b3d95ad3b6716f35d512
Desiree Alcantara <desireealcantaraa@gmail.com>
2025-01-08 14:15:40 -0800

    removed uri

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 
 // MongoDB connection URI
-//const uri = "mongodb+srv://funnyswaggame:mYnMg4B7Rvk27tMb@study.0k3d8.mongodb.net/";
 const uri = process.env.MONGO_URI;
 
 
